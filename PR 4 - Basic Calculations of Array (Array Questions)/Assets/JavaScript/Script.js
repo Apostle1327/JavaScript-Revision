062d531c08cd11789d88e8574da5265bfde80ffb
Apostle1327 <apostle1327@gmail.com>
2024-08-03 12:03:15 +0530

    Finishing the Project

--- PR 4 - Basic Calculations of Array (Array Questions)/Assets/JavaScript/Script.js
+++ PR 4 - Basic Calculations of Array (Array Questions)/Assets/JavaScript/Script.js
@@ -47,13 +47,93 @@ function question4() {
 
   for (i = 0; i < a1.length; i++) {
     for (j = 0; j < a2.length; j++) {
-      if (a1[i] == a2[j]) {
-        for (i = 0; i < a2.length; i++) {
-          a3[a3.length] = a1[i] == a2[j];
-        }
+      if (a1[i] === a2[j]) {
+        a3.push(a2[j]);
       }
     }
   }
   return console.log(a3);
 }
-function question5() {}
+function question5() {
+  let a1 = [0, 1, 0, 3, 0, 12];
+  let result = 0;
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== 0) {
+      [a1[result], a1[i]] = [a1[i], a1[result]];
+      result++;
+    }
+  }
+  return console.log(a1);
+}
+
+function question6() {
+  let a1 = [1, 3, 2, 7];
+  let sum = 0;
+
+  for (let i = 0; i < a1.length; i++) {
+    sum += a1[i];
+  }
+  return console.log(sum);
+}
+
+function question7() {
+  let a1 = [1, 2, 4, 5, 6, 7];
+
+  let min = Math.min(...a1);
+  let max = Math.max(...a1);
+  let totalSum = (max * (max + 1)) / 2 - (min * (min - 1)) / 2;
+
+  let sum = 0;
+  for (let i = 0; i < a1.length; i++) {
+    sum += a1[i];
+  }
+  return console.log(totalSum - sum);
+}
+
+function question8() {
+  let a1 = [3, 2, 7, 1];
+  console.log(`Initial Array = ${a1}`);
+  let last = a1[a1.length - 1];
+
+  for (let i = a1.length - 2; i >= 0; i--) {
+    a1[i + 1] = a1[i];
+  }
+  a1[0] = last;
+  return console.log(a1);
+}
+
+function question9() {
+  let a1 = [1, 3, 2, 7];
+  let a2 = [1, 3, 2, 7];
+  let result = false;
+
+  for (let i = 0; i < a1.length; i++) {
+    result = false;
+    for (let j = 0; j < a2.length; j++) {
+      if (a1[i] === a2[i]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  console.log(a1);
+  console.log(a2);
+  return console.log(`Anagram = ${result}`);
+}
+
+function question10() {
+  let a1 = [10, 20, 30, 40, 50];
+  let index = 30;
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] === index) {
+      console.log(`Value to be searched form the array : ${index}`);
+      return console.log(`Index of the value :${i}`);
+    }
+  }
+
+  console.log(`Value to be searched form the array : ${index}`);
+  return console.log(-1);
+}
