5878945888a3b5c6bf5f4f1a99a6a82b29eb8aaa
Apostle1327 <apostle1327@gmail.com>
2024-08-19 16:14:08 +0530

    Project Calc

---
+++ Project Calc/Assets/JavaScript/script.js
@@ -0,0 +1,40 @@
+// Headline Js
+
+//you can change the text, but you need to keep same format [yourtext, color];
+let yourtext = [["Calc", "#ffc107"]];
+
+let x = 0;
+let y = 0;
+//how fast typing is
+let wait = 200;
+//how long you want to text stay before overwriting
+let additionalwait = 1;
+let txt = yourtext[0][0].split("");
+let out = "";
+let retyping = setInterval(function () {
+  document.getElementById("changingText").innerHTML = out;
+  if (x > txt.length - 1) {
+  } else {
+    out += txt[x];
+  }
+  x++;
+  if (x == txt.length + 2 + additionalwait) {
+    if (y == yourtext.length - 1) {
+      y = 0;
+      txt = yourtext[y][0].split("");
+      out = "";
+      document.getElementById("changingText").innerHTML = out;
+      document.getElementById("changingText").style.color = yourtext[y][1];
+      x = 0;
+    } else {
+      y += 1;
+      txt = yourtext[y][0].split("");
+      out = "";
+      document.getElementById("changingText").innerHTML = out;
+      document.getElementById("changingText").style.color = yourtext[y][1];
+      x = 0;
+    }
+  }
+}, wait);
+
+// Main Calculator Js
