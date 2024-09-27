7b12024c90ca98c83aba59701f096de4683b16f7
Apostle1327 <apostle1327@gmail.com>
2024-09-27 12:28:48 +0530

    Pending Projects

---
+++ PR 9 - Slider/Assets/JavaScript/script.js
@@ -0,0 +1,5 @@
+const slides = document.querySelector(`.slides`);
+const slideCount = document.querySelectorAll(`.slides`).length;
+let currentSlide = 0;
+let intervalTime = 2000;
+let slideInterval;
